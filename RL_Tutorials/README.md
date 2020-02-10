161258b6c2800c75da77a86f33e0d39bc08d855b
Danny Kim <dannykim153@gmail.com>
2020-02-10 04:44:47 +0000

    better file structure for users to navigate through

--- RL_Tutorials/README.md
+++
@@ -1,5 +0,0 @@
-# UCL-AI-Society-RL-Tutorials
-This repository contains every material for RL Tutorials held by UCL AI Society
-
-Expecting Contents
-
--- raw_python_codes/Session1/pandasIntro.py
+++ raw_python_codes/Session1/pandasIntro.py
@@ -6,14 +6,14 @@ def main():
     # TODO: Make Series using pandas Series()
     module_score_dic = {'Database': 90, 'Security': 70, 'Math': 100, 'Machine Learning': 80}
     module_score = pd.Series(module_score_dic)
-    print(module_score, '\n')
-    print(type(module_score))
+    print("Module_score: \n", module_score, '\n')
+    print("type of module_score: \n", type(module_score))
 
     # TODO: Make DataFrame using pandas DataFrame()
     # data = pd.DataFrame(module_score, columns=['score'])
     data = pd.DataFrame(module_score, index=[x for x in module_score.keys()], columns=['score'])
-    print(data, '\n')
-    print(type(data))
+    print("data: \n", data, '\n')
+    print("type of data: \n", type(data))
 
 
 if __name__ == "__main__":
